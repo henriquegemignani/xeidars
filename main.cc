ac63bb0e82d92bb431d21b33aebd05b325586c0a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-03-12 20:07:16 -0300

    XEIDARS RUNS

--- main.cc
+++ main.cc
@@ -1,10 +1,16 @@
 #include <ugdk/base/engine.h>
 #include <ugdk/math/vector2D.h>
+#include <ugdk/action/scene.h>
+#include <ugdk/graphic/node.h>
+#include <ugdk/graphic/drawable/texturedrectangle.h>
+#include <ugdk/graphic/texture.h>
+#include <ugdk/action/generictask.h>
+#include <ugdk/input/inputmanager.h>
 #include "SDL.h"
 
-ugdk::Engine* engine() {
-    return ugdk::Engine::reference();
-}
+using namespace ugdk;
+using namespace action;
+using namespace graphic;
 
 int main(int argc, char *argv[]) {
     ugdk::Configuration engine_config;
@@ -13,9 +19,23 @@ int main(int argc, char *argv[]) {
     engine_config.fullscreen   = false;
 
     engine_config.base_path = "./";
-    engine()->Initialize(engine_config);
+    auto engine = ugdk::Engine::reference();
+    engine->Initialize(engine_config);
 
-	engine()->Run();
-    engine()->Release();
+    auto t = new Scene;
+    t->AddTask(new GenericTask(
+        [t](double) -> bool {
+            if(INPUT_MANAGER()->KeyPressed(ugdk::input::K_ESCAPE))
+                t->Finish();
+            return true;
+        }
+    ));
+    auto texture = Texture::CreateFromFile("you_win.png");
+
+    t->content_node()->set_drawable(new TexturedRectangle(texture));
+    engine->PushScene(t);
+
+	engine->Run();
+    engine->Release();
     return 0;
 }
