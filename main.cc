ea526f743877cbe28ca6929282d1dc09de564a36
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-03-13 00:46:11 -0300

    Files and simplified code

--- main.cc
+++ main.cc
@@ -1,3 +1,12 @@
+#include "GL/glew.h"
+#define NO_SDL_GLEXT
+#include "SDL_opengl.h"
+#include "SDL.h"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
 #include <ugdk/base/engine.h>
 #include <ugdk/math/vector2D.h>
 #include <ugdk/action/scene.h>
@@ -6,36 +15,105 @@
 #include <ugdk/graphic/texture.h>
 #include <ugdk/action/generictask.h>
 #include <ugdk/input/inputmanager.h>
-#include "SDL.h"
+#include <ugdk/graphic/shader/shader.h>
+#include <ugdk/graphic/shader/shaderprogram.h>
+
+namespace ugdk {
+namespace graphic {
+extern bool ConvertSurfaceToTexture(SDL_Surface* data, GLuint* texture_, int* texture_width_, int* texture_height_);
+}
+}
 
 using namespace ugdk;
 using namespace action;
 using namespace graphic;
 
+bool setupprogram(shader::ShaderProgram& program, const char* vertex, const char* fragment) {
+    shader::Shader vertex_shader(GL_VERTEX_SHADER), fragment_shader(GL_FRAGMENT_SHADER);
+
+    auto f = [](const char* filename) -> std::string {
+        std::ifstream in(filename, std::ios::in | std::ios::binary);
+        std::ostringstream contents;
+        contents << in.rdbuf();
+        in.close();
+        return(contents.str());
+    };
+
+    vertex_shader.CompileSource(f(vertex).c_str());
+    fragment_shader.CompileSource(f(fragment).c_str());
+    
+    program.AttachShader(vertex_shader);
+    program.AttachShader(fragment_shader);
+    
+    return program.SetupProgram();
+}
+
 int main(int argc, char *argv[]) {
-    ugdk::Configuration engine_config;
-    engine_config.window_title = "Xeidas";
-    engine_config.window_size  = ugdk::math::Vector2D(800, 600);
-    engine_config.fullscreen   = false;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
 
-    engine_config.base_path = "./";
-    auto engine = ugdk::Engine::reference();
-    engine->Initialize(engine_config);
+    glewInit();
 
-    auto t = new Scene;
-    t->AddTask(new GenericTask(
-        [t](double) -> bool {
-            if(INPUT_MANAGER()->KeyPressed(ugdk::input::K_ESCAPE))
-                t->Finish();
-            return true;
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+
+    shader::ShaderProgram myprogram;
+    setupprogram(myprogram, "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+    GLuint programID = myprogram.id();
+
+    static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    bool quit = false;
+    while(!quit) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_QUIT: quit = true; break;
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.sym == SDLK_ESCAPE) quit = true;
+                    break;
+                default: break;
+            }
         }
-    ));
-    auto texture = Texture::CreateFromFile("you_win.png");
+		// Clear the screen
+		glClear( GL_COLOR_BUFFER_BIT );
 
-    t->content_node()->set_drawable(new TexturedRectangle(texture));
-    engine->PushScene(t);
+		// Use our shader
+		glUseProgram(programID);
 
-	engine->Run();
-    engine->Release();
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+		glDisableVertexAttribArray(0);
+
+        SDL_GL_SwapBuffers();
+    }
+
+    glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
     return 0;
 }
