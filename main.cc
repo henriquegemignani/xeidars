240cb60e5122009c3c24be12f92fd747cb38124c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-03-13 17:53:43 -0300

    Progress in UGDK-fying the shader.

--- main.cc
+++ main.cc
@@ -57,7 +57,8 @@ bool setupprogram(shader::ShaderProgram& program, const char* vertex, const char
 
 class MahDrawable : public Drawable {
   public:
-    MahDrawable(shader::ShaderProgram* program) : program_(program) {
+    MahDrawable(shader::ShaderProgram* program, GLuint _vertexbuffer, GLuint _uvbuffer) 
+            : program_(program), vertexbuffer(_vertexbuffer), uvbuffer(_uvbuffer) {
         // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
         glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
         // Camera matrix
@@ -83,6 +84,8 @@ class MahDrawable : public Drawable {
     }
     ~MahDrawable() {
         glDeleteTextures(1, &GLTexture);
+        glDeleteBuffers(1, &vertexbuffer);
+        glDeleteBuffers(1, &uvbuffer);
     }
 
     void Update(double dt) {}
@@ -99,6 +102,36 @@ class MahDrawable : public Drawable {
 		glBindTexture(GL_TEXTURE_2D, GLTexture);
 		// Set our "myTextureSampler" sampler to user Texture Unit 0
 		glUniform1i(TextureID, 0);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// 2nd attribute buffer : UVs
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			2,                                // size : U+V => 2
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
     }
 
     const ugdk::math::Vector2D& size() const {
@@ -113,6 +146,8 @@ class MahDrawable : public Drawable {
 
     GLuint MatrixID;
     GLuint TextureID;
+
+    GLuint vertexbuffer, uvbuffer;
 };
 
 int main(int argc, char *argv[]) {
@@ -211,8 +246,6 @@ int main(int argc, char *argv[]) {
 		0.667979f, 1.0f-0.335851f
 	};
 
-    MahDrawable* mahdrawable = new MahDrawable(myprogram);
-
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -222,6 +255,8 @@ int main(int argc, char *argv[]) {
 	glGenBuffers(1, &uvbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+    
+    MahDrawable* mahdrawable = new MahDrawable(myprogram, vertexbuffer, uvbuffer);
 
     bool quit = false;
     while(!quit) {
@@ -235,48 +270,17 @@ int main(int argc, char *argv[]) {
                 default: break;
             }
         }
-		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
         mahdrawable->Draw(Geometry(), VisualEffect());
 
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// 2nd attribute buffer : UVs
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-		glVertexAttribPointer(
-			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-			2,                                // size : U+V => 2
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
-
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
-
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-
         SDL_GL_SwapBuffers();
+		
+		// Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     }
 
     delete myprogram;
 
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteBuffers(1, &uvbuffer);
 	glDeleteVertexArrays(1, &VertexArrayID);
     return 0;
 }
