32bfe02b57a9247449ad7820c6b8601f772fb9a9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-03-15 17:22:36 -0300

    Awesum code.

--- main.cc
+++ main.cc
@@ -12,18 +12,18 @@
 using namespace glm;
 
 #include <ugdk/base/engine.h>
-#include <ugdk/math/vector2D.h>
 #include <ugdk/action/scene.h>
+#include <ugdk/action/generictask.h>
+#include <ugdk/input/inputmanager.h>
+#include <ugdk/math/vector2D.h>
 #include <ugdk/graphic/videomanager.h>
 #include <ugdk/graphic/node.h>
 #include <ugdk/graphic/drawable.h>
-#include <ugdk/graphic/drawable/texturedrectangle.h>
 #include <ugdk/graphic/texture.h>
-#include <ugdk/action/generictask.h>
-#include <ugdk/input/inputmanager.h>
 #include <ugdk/graphic/opengl/shader.h>
 #include <ugdk/graphic/opengl/shaderprogram.h>
 #include <ugdk/graphic/opengl/vertexbuffer.h>
+#include <ugdk/math/integer2D.h>
 
 using namespace ugdk;
 using namespace action;
@@ -31,14 +31,15 @@ using namespace graphic;
 
 class MahDrawable : public Drawable {
   public:
-    MahDrawable(opengl::ShaderProgram* program, opengl::VertexBuffer* _vertexbuffer, opengl::VertexBuffer* _uvbuffer) 
-            : program_(program), vertexbuffer_(_vertexbuffer), uvbuffer_(_uvbuffer) {
-
-        // Load the texture using any two methods
-        texture_ = Texture::CreateFromFile("uvtemplate.tga");
+    MahDrawable(Texture* texture) : size_(math::Integer2D(texture->width(), texture->height())), texture_(texture) {
+        vertexbuffer_ = createBuffer();
+        uvbuffer_ = createBuffer();
+    }
+    MahDrawable(Texture* texture, const math::Vector2D& _size) : size_(_size), texture_(texture) {
+        vertexbuffer_ = createBuffer();
+        uvbuffer_ = createBuffer();
     }
     ~MahDrawable() {
-        delete texture_;
         delete vertexbuffer_;
         delete uvbuffer_;
     }
@@ -46,11 +47,11 @@ class MahDrawable : public Drawable {
     void Update(double dt) {}
     void Draw(const Geometry& geometry, const VisualEffect&) const {
 		// Use our shader
-        opengl::ShaderProgram::Use shader_use(program_);
+        opengl::ShaderProgram::Use shader_use(VIDEO_MANAGER()->default_shader());
 
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
-        shader_use.SendGeometry(geometry);
+        shader_use.SendGeometry(geometry * Geometry(math::Vector2D(), size_));
 
 		// Bind our texture in Texture Unit 0
         shader_use.SendTexture(0, texture_);
@@ -70,63 +71,34 @@ class MahDrawable : public Drawable {
     }
 
   private:
+    opengl::VertexBuffer* createBuffer() {
+        static const GLfloat buffer_data[] = { 
+            0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
+        };
+        opengl::VertexBuffer* buffer = opengl::VertexBuffer::Create(sizeof(buffer_data), GL_ARRAY_BUFFER, GL_STATIC_DRAW);
+        {
+            opengl::VertexBuffer::Bind bind(*buffer);
+            opengl::VertexBuffer::Mapper mapper(*buffer);
+
+            GLfloat *indices = static_cast<GLfloat*>(mapper.get());
+            if (indices)
+                memcpy(indices, buffer_data, sizeof(buffer_data));
+        }
+        return buffer;
+    }
+
     ugdk::math::Vector2D size_;
-    opengl::ShaderProgram* program_;
     Texture* texture_;
     opengl::VertexBuffer* vertexbuffer_;
     opengl::VertexBuffer* uvbuffer_;
 };
-
-opengl::VertexBuffer* make_vertex_buffer() {
-    // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = { 
-		   0.0f,-100.0f,
-		 800.0f,-100.0f,
-		 800.0f, 600.0f,
-		 200.0f, 350.0f
-	};
-	
-    opengl::VertexBuffer* buffer = opengl::VertexBuffer::Create(sizeof(g_vertex_buffer_data), GL_ARRAY_BUFFER, GL_STATIC_DRAW);
-    {
-        opengl::VertexBuffer::Bind bind(*buffer);
-        opengl::VertexBuffer::Mapper mapper(*buffer);
-
-        GLfloat *indices = static_cast<GLfloat*>(mapper.get());
-        if (indices)
-            memcpy(indices, g_vertex_buffer_data, sizeof(g_vertex_buffer_data));
-    }
-
-    return buffer;
-}
-
-opengl::VertexBuffer* make_uv_buffer() {
-	// Two UV coordinatesfor each vertex. They were created withe Blender.
-	static const GLfloat g_uv_buffer_data[] = { 
-		0.0f, 0.0f,
-		1.0f, 0.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f
-	};
-
-    opengl::VertexBuffer* buffer = opengl::VertexBuffer::Create(sizeof(g_uv_buffer_data), GL_ARRAY_BUFFER, GL_STATIC_DRAW);
-    {
-        opengl::VertexBuffer::Bind bind(*buffer);
-        opengl::VertexBuffer::Mapper mapper(*buffer);
-
-        GLfloat *indices = static_cast<GLfloat*>(mapper.get());
-        if (indices)
-            memcpy(indices, g_uv_buffer_data, sizeof(g_uv_buffer_data));
-    }
-    return buffer;
-}
 
 int main(int argc, char *argv[]) {
     auto eng = ugdk::Engine::reference();
     eng->Initialize();
     eng->video_manager()->SetVSync(true);
 
-    MahDrawable* mahdrawable = new MahDrawable(VIDEO_MANAGER()->default_shader(), make_vertex_buffer(), make_uv_buffer());
+    MahDrawable* mahdrawable = new MahDrawable(Texture::CreateFromFile("uvtemplate.tga"), math::Vector2D(800, 600));
 
     auto scene = new Scene;
     scene->content_node()->set_drawable(mahdrawable);
